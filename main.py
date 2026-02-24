29f3224f2da607687f5f6025da22d483daa4f6e8
matheswaran666 <matheswaran15082007@gmail.com>
2026-02-24 16:51:32 +0530

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from routers import ai_chat, create_collection, ai_audio_chat
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 

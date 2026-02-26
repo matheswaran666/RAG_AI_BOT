e801a4385d8bb349a04e318cf6217809eb5ca744
matheswaran666 <matheswaran15082007@gmail.com>
2026-02-26 16:35:48 +0530

    update logs

--- routers/ai_chat.py
+++ routers/ai_chat.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, HTTPException, Request, Depends
 from pydantic import BaseModel, Field
 from core.dependencies import get_llm
-
+import logging
 
 router = APIRouter()
+logger = logging.getLogger(__name__)
+
 
 class ChatRequest(BaseModel):
     prompt: str
@@ -11,13 +13,24 @@ class ChatRequest(BaseModel):
 
 
 @router.post("/ask")
-def ask(request: Request, body: ChatRequest , llm = Depends(get_llm)):
-    print("ask chat")
+def ask(request: Request, body: ChatRequest, llm=Depends(get_llm)):
+    logger.info("POST /ask called")
+    logger.debug(f"Client: {request.client}")
+    logger.debug(f"Collection (api_key): {body.api_key}")
+
     if not body.prompt:
+        logger.warning("Empty prompt received")
         raise HTTPException(status_code=400, detail="No prompt provided")
-        
+
     if not llm.collection_exists(body.api_key):
+        logger.warning(f"Collection not found: {body.api_key}")
         raise HTTPException(status_code=404, detail="Collection not found")
-        
-    response = llm.ask_llm(body.prompt, body.api_key)
-    return {"response": response}
+
+    try:
+        response = llm.ask_llm(body.prompt, body.api_key)
+        logger.info("LLM response generated successfully")
+        return {"response": response}
+
+    except Exception:
+        logger.exception("Error during LLM processing")
+        raise HTTPException(status_code=500, detail="Internal server error")
